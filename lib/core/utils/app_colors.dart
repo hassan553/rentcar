966234e40ea670d2f7325f90f195eda10c12bb55
hassan553 <hassanmarzouk042@gmail.com>
2024-08-09 12:24:54 -0700

    first commit

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color white = Colors.white;
+  static const Color primary = Color(0xffE6FE58);
+  static const Color lightBlue = Color(0xff9DB2CE);
+  static const Color black = Color(0xff141414);
+  static const Color lightBlack = Color(0xff131313);
+  static const Color orange = Color(0xffFFD233);
+  static const Color lightOrange = Color(0xffF7FFC8);
+}
